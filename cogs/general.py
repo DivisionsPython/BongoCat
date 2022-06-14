4ce0bd233bebd8f31198b46a9efcedc179041087
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2022-06-14 15:36:10 +0200

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -8,11 +8,20 @@ class General(commands.Cog):
 
     @commands.command()
     async def ping(self, ctx):
-        await ctx.channel.send(f'\U0001f4e1 My latency is **{round(self.bot.latency * 1000)}ms**')
+        embed = discord.Embed()
+        embed.title = f'\U0001f4e1 My latency is **{round(self.bot.latency * 1000)}ms**'
+        embed.color = 0xdda7ff
+        await ctx.channel.send(embed=embed)
 
     @commands.command()
     async def invite(self, ctx):
-        await ctx.channel.send(f"**Invite me**: https://discord.com/api/oauth2/authorize?client_id={self.bot.user.id}&permissions=8&scope=bot")
+        embed = discord.Embed()
+        embed.title = "Click the link! \U0001f447"
+        embed.color = 0xdda7ff
+        embed.set_thumbnail(url=self.bot.user.avatar_url)
+        embed.add_field(name="»»————-   ★   ————-««",
+                        value=f"https://discord.com/api/oauth2/authorize?client_id={self.bot.user.id}&permissions=8&scope=bot")
+        await ctx.channel.send(embed=embed)
 
 
 def setup(bot):
