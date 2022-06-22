30b290355dd15a2af2846cc90da401b6245ec98b
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2022-06-22 19:52:32 +0200

    Update general.py
    - Better 'invite' command using 'discord.ui.Button'

--- cogs/general.py
+++ cogs/general.py
@@ -1,5 +1,8 @@
 import discord
 from discord.ext import commands
+import datetime
+from discord.ui import Button, View
+from discord import ButtonStyle
 
 
 class General(commands.Cog):
@@ -15,12 +18,30 @@ class General(commands.Cog):
 
     @commands.command()
     async def invite(self, ctx):
+        button = Button(
+            label='Invite', url=f"https://discord.com/api/oauth2/authorize?client_id={self.bot.user.id}&permissions=8&scope=bot", style=ButtonStyle.url)
+        view = View()
+        view.add_item(button)
+
         embed = discord.Embed()
-        embed.title = "Click the link! \U0001f447"
+        embed.title = f"Click the button below to invite me to your server! \U0001f389"
         embed.color = 0xdda7ff
-        embed.set_thumbnail(url=self.bot.user.avatar.url)
-        embed.add_field(name="»»————-   ★   ————-««",
-                        value=f"https://discord.com/api/oauth2/authorize?client_id={self.bot.user.id}&permissions=8&scope=bot")
+        await ctx.channel.send(embed=embed, view=view)
+
+    @commands.command(aliases=["av", "pfp"])
+    async def avatar(self, ctx, member: discord.Member = None):
+        if not member:
+            member = ctx.author
+
+        embed = discord.Embed()
+        embed.title = f"{member.name}'s avatar"
+        embed.add_field(name="Download image",
+                        value=f"[Click here]({member.avatar.url})")
+        embed.set_image(url=str(member.avatar.url))
+        embed.set_footer(
+            text=f"Requested by {ctx.author.name}", icon_url=str(ctx.author.avatar.url))
+        embed.timestamp = datetime.datetime.now()
+        embed.color = 0xdda7ff
         await ctx.channel.send(embed=embed)
 
 
